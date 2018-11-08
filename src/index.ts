24d0c7c025a2aea4b130bfac9deb26304b3fdc9d
Filip Andersson <filipandersson.1997.245@gmail.com>
2018-11-08 11:18:53 +0100

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (_req, res) => res.send("Hello world"));
+
+app.listen(port, () => console.log("started!"));
