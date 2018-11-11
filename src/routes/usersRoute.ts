ac3eb51ff467459e26c533647f0c421f2a6c12e3
Filip <jabos123@gmail.com>
2018-11-11 21:00:36 +0100

    Added all rotues for users.

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -10,4 +10,20 @@ userRouter.post("/", (_req, res) => {
   res.send(200);
 });
 
+userRouter.delete("/{userId}", async (_req, res) => {
+  res.send(200);
+});
+
+userRouter.put("/{userId}", async (_req, res) => {
+  res.send(200);
+});
+
+userRouter.post("/login", async (_req, res) => {
+  res.send(200);
+});
+
+userRouter.post("/logout", async (_req, res) => {
+  res.send(200);
+});
+
 export default userRouter;
