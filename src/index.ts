6b841ae14b03fd2530d53fc4cde6200bef27b89a
Filip Andersson <filipandersson.1997.245@gmail.com>
2018-11-12 14:51:23 +0100

    Added logging and parsing of body and removed test route.

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,14 @@ import { Users } from "./db/entity/users";
 import votesRouter from "./routes/votesRouter";
 import tagsRouter from "./routes/tagsRouter";
 import recipesRouter from "./routes/recipesRouter";
+import bodyparser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", async (_req, res) => {
-  const connection = await GetConnection();
-  const user = new Users();
-  user.Email = "123@gmail.com";
-  user.hash = "adapjosfoidsvo5";
-  user.Nickname = "Tommy";
-  await connection.manager.save(user).catch((err) => console.error(err));
-  res.send("Hello world");
-});
-
+app.use(morgan("dev"));
+app.use(bodyparser.json());
 app.use("/users", userRouter);
 app.use("/votes", votesRouter);
 app.use("/comments", votesRouter);
